7114abf86741d4c4c13590bc73ba8e54e25fe579
Chad Chambers <61452826+CMChambers@users.noreply.github.com>
2026-02-12 23:13:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReusableUserInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,11 @@
+﻿namespace ReusableUserInterface
+{
+    internal class UserInterface
+    {
+        // greeting
+
+        // main menu
+
+
+    }
+}
